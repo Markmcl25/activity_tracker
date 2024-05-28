fa8bc2053c7d097742822d9ce7af642ea7fa3ddd
Mark Mcloughlin <mark25mky@gmail.com>
2024-05-28 00:26:56 +0000

    added python code including bottle module for framework

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from bottle import Bottle, request, template
+
+app = Bottle()
+
+# Define routes
+@app.route('/')
+def index():
+    return template('index')
+
+@app.route('/submit', method='POST')
+def submit():
+    # Get data from the form
+    data = request.forms.get('data')
+
+    # TODO: Write code to interact with Google Spreadsheet
+
+    return "Data submitted successfully!"
+
+if __name__ == '__main__':
+    # Run the Bottle app
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
